7ccd07f53248eabec87d7b633361f38685147917
aditya6677 <6677aditya@gmail.com>
2024-04-21 12:08:08 +0530

    Design

---
+++ Design/Singleton/Client.java
@@ -0,0 +1,9 @@
+package Design.Singleton;
+
+public class Client {
+    public static void main(String[] args) {
+        Singleton singleton = new Singleton();
+        singleton.hello();
+
+    }
+}
---
+++ Design/Singleton/Singleton.java
@@ -0,0 +1,10 @@
+package Design.Singleton;
+
+public class Singleton {
+
+    public Singleton(){}
+
+    void hello(){
+        System.out.println("Hello World");
+    }
+}
