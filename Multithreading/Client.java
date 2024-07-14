b1d4d6d4c8992d07e88310054ae08a326fca9664
aditya6677 <6677aditya@gmail.com>
2024-07-14 21:00:43 +0530

    Multithreading using functional interfaces

--- Multithreading/Client.java
+++ Multithreading/Client.java
@@ -1,13 +1,21 @@
 package Multithreading;
 
+import java.nio.file.FileAlreadyExistsException;
+
 public class Client {
     public static void main(String[] args) {
         T1 t1 = new T1();
         t1.start();
 
-        T2 t2 = new T2();
-        Thread t3 = new Thread(t2);
-        t3.start();
+        try{
+            T2 t2 = new T2();
+            Thread t3 = new Thread(t2);
+            t3.start();
+        }
+        catch (NullPointerException nullPointerException){
+            System.out.println(nullPointerException.getMessage());
+        }
+
 
         Runnable runnable = () -> System.out.println("Hey Runnable");
 
